1f8fb047d6829e4bc92db8c08e86baba1dcc3b70
asilbek2009 <asilbeksamadov969@gmail.com>
2021-09-09 13:29:39 +0500

    inital commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var intro = alert("xush kelibsz bizning saytga!");
+var firstName = prompt("Ismingiz!", "");
+var surName = prompt("Familyangiz!", "");
+var age = prompt("Yoshingiz!", "");
+console.log(`Ismingiz: ${firstName}, Familyangiz: ${surName}, Yoshingiz: ${age}`);
